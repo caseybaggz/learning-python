8044306b9bbad934658e46c22ca04b9040a27ae3
Casey Baggz <caseybaggz@gmail.com>
2021-04-16 09:31:44 -0500

    feat(01): submit exercise result

--- src/exercise/01/01.py
+++ src/exercise/01/01.py
@@ -1,25 +1,60 @@
 import random
 
-choice_options = {
-  '1': 'ROCK',
-  '2': 'PAPER',
-  '3': 'SCISSORS'
+ROCK = 'ROCK'
+PAPER = 'PAPER'
+SCISSORS = 'SCISSORS'
+
+options = {
+  ROCK: {
+    'id': '1',
+    'label': 'Rock'
+  },
+  PAPER: {
+    'id': '2',
+    'label': 'Paper'
+  },
+  SCISSORS: {
+    'id': '3',
+    'label': 'Scissors'
+  }
 }
 
-choices = []
-for v in choice_options.values():
-  choices.append(v)
+def setup_chioces():
+  choices = []
+  for option_item in options.keys():
+    choices.append(options[option_item].get('id'))
+  return choices
 
-ai = random.choice(choices)
-user = input('ROCK, PAPER, SCISSORS!!!!\nChoose Your Option...\n\n1. Rock\n2. Paper\n3. Scissors\n')
+def start_game(choices):
+  ai = random.choice(choices)
+  user = input('ROCK, PAPER, SCISSORS!!!!\nChoose Your Option...\n\n1. Rock\n2. Paper\n3. Scissors\n')
+  return {
+    'ai': ai,
+    'user': user
+  }
 
-# 1. Define a function that takes in both choices
-# 2. Return a boolean depending on if user wins
-# 3. Define a function that returns string result based on win status
-# 4. Print win result function
+def determine_win(players, choices):
+  user = players.get('user')
+  ai = players.get('ai')
 
-if user not in choices:
-  print('You must choose either 1, 2, or 3 as an option.')
+  if user not in choices:
+    print('You must choose either 1, 2, or 3 as an option.')
+    return
 
-if ai == user:
-  print('Tie!')
+  if ai == user:
+    print('Tie!')
+  elif ai == options.get(ROCK)['id'] and user == options.get(SCISSORS)['id']:
+    print('Computer wins...')
+  elif ai == options.get(SCISSORS)['id'] and user == options.get(PAPER)['id']:
+    print('Computer wins...')
+  else:
+    print('YOU WIN!')
+
+  print('You chose ' + user + '. The computer chose ' + ai + '.');
+
+# Run Game
+
+choices = setup_chioces()
+players = start_game(choices)
+
+determine_win(players, choices)
